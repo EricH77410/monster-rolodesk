7e559d9a2ac6a9622d04c199b7e77342d7c1d0a7
EricH77410 <rico@club-internet.fr>
2020-03-08 10:25:08 +0100

    Updates

---
+++ precache-manifest.3762ad937acde9ffe3a415c0280192dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767801717105daf808800166505a67cb",
+    "url": "/monster-rolodesk/index.html"
+  },
+  {
+    "revision": "7b7e977c7dc3641d4eb3",
+    "url": "/monster-rolodesk/static/css/main.8b7f1206.chunk.css"
+  },
+  {
+    "revision": "e6258cbbe686f7ab5cff",
+    "url": "/monster-rolodesk/static/js/2.9bf61b77.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodesk/static/js/2.9bf61b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b7e977c7dc3641d4eb3",
+    "url": "/monster-rolodesk/static/js/main.91e3ef2c.chunk.js"
+  },
+  {
+    "revision": "ea16e54fb7eb53fb2898",
+    "url": "/monster-rolodesk/static/js/runtime-main.c67a4983.js"
+  }
+]);
